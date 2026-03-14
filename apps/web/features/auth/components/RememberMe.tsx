f595e54487017c9eb770b742cac14327e0d910e3
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2026-03-15 01:53:55 +0600

    style: update style

--- apps/web/features/auth/components/RememberMe.tsx
+++ apps/web/features/auth/components/RememberMe.tsx
@@ -1,12 +1,12 @@
-import { Field, FieldLabel } from "@workspace/ui/components/field";
-import { Checkbox } from "@workspace/ui/components/checkbox";
-import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Field, FieldLabel } from "@workspace/ui/components/field"
+import { Checkbox } from "@workspace/ui/components/checkbox"
+import { Control, Controller, FieldValues, Path } from "react-hook-form"
 
 interface RememberMeProps<
   TFieldValues extends FieldValues,
 > extends React.ComponentProps<"input"> {
-  name: Path<TFieldValues>;
-  control: Control<TFieldValues>;
+  name: Path<TFieldValues>
+  control: Control<TFieldValues>
 }
 
 export default function RememberMe<TFieldValues extends FieldValues>({
@@ -22,6 +22,7 @@ export default function RememberMe<TFieldValues extends FieldValues>({
         <Field orientation="horizontal" aria-disabled={disabled}>
           <Checkbox
             id="remember-me"
+            className="size-4 rounded"
             name={field.name}
             checked={field.value}
             onCheckedChange={field.onChange}
@@ -37,5 +38,5 @@ export default function RememberMe<TFieldValues extends FieldValues>({
         </Field>
       )}
     />
-  );
+  )
 }
